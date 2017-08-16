6f5ac5d31a3e82f6d7cda48394a2f121ff7f4c2b
staceyspikesgit config --global user.name staceyspikes <stacey.lynne13@yahoo.com>
2017-08-15 20:50:41 -0400

    Created more variable types

--- VariablesOperatorsExpressions/Program.cs
+++ VariablesOperatorsExpressions/Program.cs
@@ -10,6 +10,60 @@ namespace VariablesOperatorsExpressions
     {
         static void Main(string[] args)
         {
+            string name = "Stacey";
+            Console.WriteLine(name);
+            // case matters when naming variables
+            name = "Dave";
+            Console.WriteLine(name);
+            
+            string birthMonth = "June";
+            Console.WriteLine(birthMonth);
+            // integers
+            int birthMonthNumber = 14;
+            int age = 22;
+            int populatioin = 33333;
+            // floating type variables
+            float number = 2.164403304897294383961032f;
+            double someNumber = 2.164403304897294383961032d;
+            decimal partialNumber = 2.164403304897294383961032m;
+
+            //Console.WriteLine(number);
+            //Console.WriteLine(someNumber);
+            //Console.WriteLine(partialNumber);
+
+
+            //Boolean
+            bool isPresent = true;
+            bool seatTaken = false;
+
+            //Character
+            char lastLetter = 's';
+
+
+            int jessicaAge = 23;
+            int samAge = 47;
+            Console.WriteLine(jessicaAge > samAge);
+
+            int mike = 13;
+            int friend1 = 13;
+            int friend2 = 13;
+            Console.WriteLine(mike + friend1 + friend2);
+
+
+
+
+
+
+
+
+
+
+       
+
+
+
+
+
         }
     }
 }
